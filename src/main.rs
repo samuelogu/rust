e3f4bfa63610546e860b8af5440015c6f1dbe700
Samuel Ogu <hello@samuelogu.com>
2021-08-14 23:42:26 +0100

    functions

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@
 // mod arrays;
 // mod vectors;
 // mod conditionals;
-mod loops;
+// mod loops;
+mod functions;
 
 fn main() {
     // strings::run();
@@ -14,5 +15,6 @@ fn main() {
     // arrays::run();
     // vectors::run();
     // conditionals::run();
-    loops::run();
+    // loops::run();
+    functions::run();
 }
